bdcaa4fe5283633bed8de4594e0be3b037aa63c2
TCarmine <kappa85t@hotmail.it>
2021-11-18 10:13:30 +0100

    adding dinamically posts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -9,15 +9,10 @@ export const BlogProvider = ({children}) => {
   // setBlogPosts replace the complete array
   const addBlogPost = () => {
     // check how to add dinamic id
-    setBlogPosts([...blogPosts, {title:`Blog Post #${blogPosts.length+1}`, id:`${blogPosts.length+1}`} ])
+    setBlogPosts([
+      ...blogPosts, 
+      {title:`Blog Post #${blogPosts.length+1}`, id:`${blogPosts.length+1}`} ])
   }
-
-/*   const blogPosts = [
-    {title: "Blog Post #1", id:1},
-    {title: "Blog Post #2", id:2},
-    {title: "Blog Post #3", id:3},
-    {title: "Blog Post #4", id:4},
-  ] */
 
   return (
     <BlogContext.Provider value={{data: blogPosts, addBlogPost}}>
