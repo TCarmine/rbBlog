0db3f517d1d6538526f0e17415b3ee06bc4c1ebb
TCarmine <kappa85t@hotmail.it>
2021-11-21 15:37:24 +0100

    save new Post

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,7 +3,14 @@ import createDataContext from "./createDataContext"
 const blogReducer = (state, action) => { 
   switch(action.type) {
     case "add_blogpost":
-      return [ ...state, {title:`Blog Post #${state.length+1}`, id: Math.floor(Math.random()*10000)}]
+      return [ 
+        ...state, 
+        {
+          id: Math.floor(Math.random()*10000),
+          title: action.payload.title,
+          content: action.payload.content
+        }
+      ]
     case "delete_blogpost":
       return state.filter( blogPost => blogPost.id !== action.payload)
     default:
@@ -12,8 +19,8 @@ const blogReducer = (state, action) => {
 }  
 
 const addBlogPost = (dispatch) => {
-  return () =>{
-    dispatch({type:"add_blogpost"})
+  return (title, content) =>{
+    dispatch({type:"add_blogpost", payload:{title:title, content:content}})
   }       
 }
 
