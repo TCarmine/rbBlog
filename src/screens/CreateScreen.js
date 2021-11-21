202cf455332434bbdcf45a3b0cb618438e2777a0
TCarmine <kappa85t@hotmail.it>
2021-11-21 21:59:36 +0100

    correct navigation adfetr blog creation

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -22,8 +22,10 @@ const CreateScreen = ({ navigation }) => {
         <Button 
           title="Add Blog Post"
           onPress={()=>{ 
-            addBlogPost(title, content)
-            navigation.navigate("Index")
+            addBlogPost(title, content, () => {
+              navigation.navigate("Index")
+            })
+            
           }}
         />
     </View>  
