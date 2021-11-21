c183f501df861f2e6c55202a1ae35d8f60142194
TCarmine <kappa85t@hotmail.it>
2021-11-21 15:26:04 +0100

    style and Button

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from "react"
-import { View, Text, TextInput, StyleSheet } from "react-native"
+import { View, Text, TextInput, StyleSheet, Button } from "react-native"
 import { Context } from "../context/BlogContext"
 
 const CreateScreen = ({ navigation }) => {
@@ -8,14 +8,35 @@ const CreateScreen = ({ navigation }) => {
   
   return (
     <View>
-      <Text>Enter Title</Text>
-      <TextInput value={title} onChangeText={(text)=>setTitle(text)}/>
-      <Text>Enter Content</Text>
-      <TextInput value={content} onChangeText={(text)=>setContent(text)}/>
+      <Text style={styles.label}>Enter Title</Text>
+      <TextInput
+        style={styles.input} 
+        value={title} 
+        onChangeText={(text)=>setTitle(text)}/>
+      <Text style={styles.label}>Enter Content</Text>
+      <TextInput 
+        style={styles.input}
+        value={content} 
+        onChangeText={(text)=>setContent(text)}/>
+        <Button title="Add Blog Post" />
     </View>  
   )  
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  input:{
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom:15,
+    padding:5,
+    margin:5
+  },
+  lable:{
+    fontSize:20,
+    marginBottom:5,
+    marginLeft:5
+  }
+})
 
 export default CreateScreen
