6adf31c1c2198999195094aae8a7ffdfd742a060
TCarmine <kappa85t@hotmail.it>
2021-11-22 10:53:08 +0100

    editing but no saving

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -11,6 +11,14 @@ const blogReducer = (state, action) => {
           content: action.payload.content
         }
       ]
+    case "edit_blogpost":
+      return state.map((blogPost) => { 
+        if(blogPost.id !== action.payload.id){
+          return action.payload
+        }else{
+          return blogPost
+        }
+      })    
     case "delete_blogpost":
       return state.filter( blogPost => blogPost.id !== action.payload)
     default:
@@ -18,22 +26,36 @@ const blogReducer = (state, action) => {
   }
 }  
 
-const addBlogPost = (dispatch) => {
+const addBlogPost = dispatch => {
   return (title, content, callback) =>{
     dispatch({type:"add_blogpost", payload:{title:title, content:content}})
-    callback()
+    if(callback){
+      callback()
+    }  
   }       
 }
 
-const deleteBlogPost = (dispatch) => {
+const deleteBlogPost = dispatch => {
   return (id) =>{
     dispatch({type:"delete_blogpost", payload: id})
   }       
 }
 
+const editBlogPost = dispatch =>{
+  return (id, title, content, callback) =>{
+    dispatch({
+      type:"edit_blogpost", 
+      payload:{id, title, content}
+    })
+    if(callback){
+      callback()
+    }   
+  }
+}  
+
 export const { Context, Provider } = createDataContext( 
   blogReducer, 
-  { addBlogPost, deleteBlogPost },
+  { addBlogPost, deleteBlogPost, editBlogPost },
   [{title:"Test Post", content:"Content test", id:1}] 
 )
 
