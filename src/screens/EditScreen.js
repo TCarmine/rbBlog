25069758a4f8be81ec46a01afd3f9686826ae512
TCarmine <kappa85t@hotmail.it>
2021-11-22 07:31:58 +0100

    Edit screen

---
+++ src/screens/EditScreen.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { View, Text, StyleSheet} from "react-native"
+import { Context } from "../context/BlogContext"
+
+const EditScreen = () => {
+  return (
+    <View>
+      <Text>
+        Edit Screen
+      </Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default EditScreen
