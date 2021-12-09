b02fce9225527784a26ffa30b497d68062e16eb3
TCarmine <kappa85t@hotmail.it>
2021-12-09 08:18:00 +0100

    cleaning

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -5,15 +5,6 @@ const blogReducer = (state, action) => {
   switch(action.type) {
     case "get_blogposts":
       return action.payload
-    /* case "add_blogpost":
-      return [ 
-        ...state, 
-        {
-          id: Math.floor(Math.random()*10000),
-          title: action.payload.title,
-          content: action.payload.content
-        }
-      ] */
     case "edit_blogpost":
         return [
           ...(state.filter( blogPost => blogPost.id !== action.payload.id )),
@@ -56,7 +47,8 @@ const deleteBlogPost = dispatch => {
 }
 
 const editBlogPost = dispatch =>{
-  return (id, title, content, callback) =>{
+  return async (id, title, content, callback) =>{
+    await jsonServer.put(`/blogposts/${id}`, { title, content })
     dispatch({
       type:"edit_blogpost", 
       payload:{id, title, content}
